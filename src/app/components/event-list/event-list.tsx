840f68483f235272be4e9d0ff674a56184d7b1c6
combolock1213 <combolock1213@gmail.com>
2026-04-13 14:49:00 +0000

    likes initial ui

--- src/app/components/event-list/event-list.tsx
+++ src/app/components/event-list/event-list.tsx
@@ -1,25 +1,50 @@
+"use client";
+
 import { Event } from "@/app/lib/definitions";
 import { formatTimeRange } from "@/app/lib/time";
 import { removePastEvents, generateEventSchedule, DaySchedule } from "../../lib/eventDisplay";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function EventList({ events }: { events: Event[] }) {
+  const [likedEvents, setLikedEvents] = useState<Set<string>>(new Set());
 
   let upcomingEvents = removePastEvents(events);
   let eventSchedule = generateEventSchedule(upcomingEvents);
+
+  const toggleLike = (eventName: string) => {
+    setLikedEvents(prev => {
+      const newSet = new Set(prev);
+      if (newSet.has(eventName)) {
+        newSet.delete(eventName);
+      } else {
+        newSet.add(eventName);
+      }
+      return newSet;
+    });
+  };
 
   return (
     <>
       <div className="">
         {eventSchedule.map((daySchedule: DaySchedule) => (
-          <EventListDay daySchedule={daySchedule} key={daySchedule.dayDisplay} />
+          <EventListDay
+            daySchedule={daySchedule}
+            key={daySchedule.dayDisplay}
+            likedEvents={likedEvents}
+            onToggleLike={toggleLike}
+          />
         ))}
       </div>
     </>
   );
 }
 
-function EventListDay({ daySchedule }: { daySchedule: DaySchedule }) {
+function EventListDay({ daySchedule, likedEvents, onToggleLike }: {
+  daySchedule: DaySchedule;
+  likedEvents: Set<string>;
+  onToggleLike: (eventName: string) => void;
+}) {
 
   return (
     <>
@@ -30,7 +55,11 @@ function EventListDay({ daySchedule }: { daySchedule: DaySchedule }) {
         <ul className="text-sm list-none">
           {daySchedule.events.map((event: Event, index: any) => (
             <li key={index} className="flex items-start mb-6 min-w-0">
-              <EventDisplay event={event} />
+              <EventDisplay
+                event={event}
+                isLiked={likedEvents.has(event.name)}
+                onToggleLike={() => onToggleLike(event.name)}
+              />
             </li>
           ))}
         </ul>
@@ -41,7 +70,11 @@ function EventListDay({ daySchedule }: { daySchedule: DaySchedule }) {
 
 
 
-function EventDisplay({ event }: { event: Event }) {
+function EventDisplay({ event, isLiked, onToggleLike }: {
+  event: Event;
+  isLiked: boolean;
+  onToggleLike: () => void;
+}) {
   let timeDisplay = formatTimeRange(event);
   const content = (
     <>
@@ -61,6 +94,17 @@ function EventDisplay({ event }: { event: Event }) {
           )}
         </div>
       </div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          onToggleLike();
+        }}
+        className="ml-2 p-1 hover:bg-gray-100 rounded transition-colors"
+        aria-label={isLiked ? "Unlike event" : "Like event"}
+      >
+        <HeartIcon filled={isLiked} />
+      </button>
     </>
   );
 
@@ -69,7 +113,9 @@ function EventDisplay({ event }: { event: Event }) {
       {content}
     </Link>
   ) : (
-    content
+    <div className="flex items-start w-full">
+      {content}
+    </div>
   );
 };
 
@@ -85,6 +131,21 @@ function LinkIcon() {
     >
       <path fillRule="evenodd" d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Z" clipRule="evenodd" />
       <path fillRule="evenodd" d="M6.194 12.753a.75.75 0 0 0 1.06.053L16.5 4.44v2.81a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.553l-9.056 8.194a.75.75 0 0 0-.053 1.06Z" clipRule="evenodd" />
+    </svg>
+  );
+}
+
+function HeartIcon({ filled }: { filled: boolean }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill={filled ? "currentColor" : "none"}
+      stroke="currentColor"
+      strokeWidth="2"
+      className={`size-4 ${filled ? "text-red-500" : "text-gray-400"}`}
+    >
+      <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
     </svg>
   );
 }
